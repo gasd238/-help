5f4d93818b6d5fbdd627fe6d8c3ca21565e0a83d
H_Apple |A.S.A.P| <39252325+HumanAdjust@users.noreply.github.com>
2019-08-28 09:06:14 +0900

    Login Function Add
    Require module "express-session" for all Back-End Developers

---
+++ models/DB.js
@@ -0,0 +1,41 @@
+const MongoClient = require('mongodb').MongoClient;
+const dbName = 'help'; // Database Name
+
+var database;
+
+exports.connectDB = function () {
+    var databaseURL = 'mongodb://localhost:27017';
+    MongoClient.connect(databaseURL, { useNewUrlParser: true },
+        function (err, client) {
+            if (err) {
+                console.log('MongoDB 접속 실패: ', err);
+                return;
+            }
+            database = client.db(dbName);
+            console.log('MongoDB 접속 성공: ' + databaseURL);
+        }
+    );
+}; //DB 연결
+
+exports.authUser = function (id, password, callback) {
+    var members = database.collection('members');
+    var result = members.find({ "id" : id, "passwords" : password });
+
+    result.toArray(
+        function (err, data) {
+            if (err) {
+                callback(err, null);
+                return;
+            }
+
+            if (data.length > 0) {
+                callback(null, data);
+            }
+            else {
+                callback(null, null);
+            }
+        }
+
+    );
+
+}; //로그인
