b01ce790bc91b30a18a688467bb776d570fae905
ParkJongHyo <s18052@gsm.hs.kr>
2019-08-28 18:03:59 +0900

    폰트 삭제, mongodb 경고문 없앰, 만들 ejs 미리생성

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ var db = require('../models/DB')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'test'});
+  res.render('index', {title: '!help'});
 }); //메인 화면(로그인 페이지)
 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+}); //메인 화면(로그인 페이지)
 module.exports = router;
