c1765f21faabb0c1ccad56cf895e49efcf6c5bc0
gasd238 <gasd238@naver.com>
2019-08-29 23:06:20 +0900

    글쓰기 관련 기능 추가

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,19 @@ var express = require('express');
 var router = express.Router();
 var db = require('../models/DB')
 
+//글 불러오기
+var post = db.collection('post')
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  if (req.session.user_id != null) {
-    res.render('../views/index.ejs', { islogin: 'login' });
-  } else {
-    res.render('../views/index.ejs', { islogin: 'no' });
-  }
+  post.find({}).toArray((err, posts)=>{
+    if (req.session.user_id != null) {
+      res.render('../views/index.ejs', { islogin: 'login', post: posts});
+    } else {
+      res.render('../views/index.ejs', { islogin: 'no', post: posts});
+    }
+  })
 }); //메인 화면(로그인 페이지)
 
 router.get('/findpw', function (req, res) {
@@ -26,4 +32,18 @@ router.get('/login', function (req, res) {
 router.get('/Mypage', function(req, res){
   res.render('../views/User/MyPage.ejs');
 });
+
+router.post('/writebooks', (req, res)=>{
+  try{
+    var date = new Date();
+    var writedate = String(date.getFullYear()) + '년 ' + String(date.getMonth() + 1) + '월 ' + String(date.getDate()) + '일';
+    var writepost = {title : req.body.title, writedate : writedate, name: req.session.name, post:req.body.post, field: req.body.field, town: req.body.town}
+    post.insertOne(writepost);
+    res.status(200).send('<script type="text/javascript">alert("글쓰기가 완료되었습니다."); document.location.href="/";</script>');
+  }
+  catch(exception){
+    res.status(401).send('<script type="text/javascript">alert("글쓰기를 실패하였습니다."); document.location.href="/write";</script>');
+  }
+})
+
 module.exports = router;
