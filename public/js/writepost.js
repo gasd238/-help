c5292bfd921506dccd0740a3e05861366b5e7c02
JEONG-MW <fwwfly@gmail.com>
2019-08-31 05:10:44 +0900

    ,
    ㅁㅁ

---
+++ public/js/writepost.js
@@ -0,0 +1,32 @@
+
+
+function getLocation() {
+  if (navigator.geolocation) {
+    //위치 정보를 얻기
+    navigator.geolocation.getCurrentPosition (function(pos) {
+        $('#latitude').html(pos.coords.latitude);     // 위도
+        $('#longitude').html(pos.coords.longitude); // 경도
+    });
+  // if (navigator.geolocation) {
+  //   navigator.geolocation.getCurrentPosition(currentPosition, geoError);
+  // }
+  // else {
+  //   w.innerHTML = "지리위치를 지원하지 않는 브라우저 입니다.";
+  // }
+}
+function currentPosition(myPosition) {
+  var a = myPosition.coords.latitude + "," + myPosition.coords.longitude;
+  var b = "http://maps.googleapis.com/maps/api/staticmap?center=" + a + "&zoom=10&size=480x320&sensor=false";
+  document.getElementById("x").innerHTML = "<img src='" + b + "'>";
+}
+function geoError(myError) {
+  switch (myError.code) {
+    case myError.PERMISSION_DENIED: w.innerHTML = "사용자가 위치정보 사용요청을 거절하였습니다."
+      break;
+    case myError.POSITION_UNAVAILABLE: w.innerHTML = "위치 정보가 사용 가능하지 않습니다."
+      break; case myError.TIMEOUT: w.innerHTML = "사용자 위치정보를 얻기 위한 요청이 타임아웃 되었습니다."
+      break; case myError.UNKNOWN_ERROR: w.innerHTML = "알수 없는 오류가 발생하였습니다."
+      break;
+  }
+}
+
