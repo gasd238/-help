48e7493a238e52365d375adf76d95c472e666077
H_Apple |A.S.A.P| <39252325+HumanAdjust@users.noreply.github.com>
2019-08-30 22:58:28 +0900

    게시판 완성(디자인 필요)

---
+++ models/writeDB.js
@@ -0,0 +1,61 @@
+const MongoClient = require('mongodb').MongoClient;
+const dbName = 'help'; // Database Name
+
+var database;
+
+exports.connectDB = function () {
+    var databaseURL = 'mongodb://localhost:27017';
+    MongoClient.connect(databaseURL, { useNewUrlParser: true, useUnifiedTopology: true },
+        function (err, client) {
+            if (err) {
+                console.log('MongoDB 접속 실패: ', err);
+                return;
+            }
+            database = client.db(dbName);
+            console.log('MongoDB 접속 성공: ' + databaseURL);
+        }
+    );
+}; //DB 연결
+
+exports.getpost = function(callback){
+    var post = database.collection('post');
+    var result = post.find({});
+
+    result.toArray(
+        function (err, data) {
+            if (err) {
+                callback(err, null);
+                return;
+            }
+
+            if (data.length > 0) {
+                callback(null, data);
+            }
+            else {
+                callback(null, null);
+            }
+        }
+
+    );
+}; //글 가져오기
+
+exports.addpost = function (title, date, name, post, field, town, callback) {
+    var posts = database.collection('post');
+
+    posts.insertMany([{ "title": title, "writedate": date, "name": name, "post": post, "field": field, "town": town }],
+        function (err, result) {
+            if (err) {
+                callback(err, null);
+                return;
+            }
+
+            if (result.insertedCount > 0) {
+                callback(null, result);
+            }
+            else {
+                callback(null, null);
+            }
+
+        }
+    );
+}; //글 작성하기
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../models/loginDB')
-var database
-var post
-
-//글 불러오기
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
-
-MongoClient.connect(url, function(err, client){
-  database = client.db('help');
-  post = database.collection('post');
-}); 
+var db = require('../models/writeDB');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  post.find({}).toArray((err, posts)=>{
+  db.getpost(
+    function (err, data) {
     if (req.session.user_id != null) {
-      res.render('../views/index.ejs', { islogin: 'login', post: posts});
+      res.render('../views/index.ejs', { islogin: 'login', post: data});
     } else {
-      res.render('../views/index.ejs', { islogin: 'no', post: posts});
+      res.render('../views/index.ejs', { islogin: 'no', post: data});
     }
-  })
+  });
 }); //메인 화면(로그인 페이지)
 
 router.get('/findpw', function (req, res) {
@@ -68,17 +58,57 @@ router.get('/writebooks', function(req, res){
   res.render('../views/Post/writepost.ejs');
 });
 
-router.post('/writebooks', (req, res)=>{
-  try{
-    var date = new Date();
-    var writedate = String(date.getFullYear()) + '년 ' + String(date.getMonth() + 1) + '월 ' + String(date.getDate()) + '일';
-    var writepost = {title : req.body.title, writedate : writedate, name: req.session.name, post:req.body.post, field: req.body.field, town: req.body.town}
-    post.insertOne(writepost);
-    res.status(200).send('<script type="text/javascript">alert("글쓰기가 완료되었습니다."); document.location.href="/";</script>');
-  }
-  catch(exception){
-    res.status(401).send('<script type="text/javascript">alert("글쓰기를 실패하였습니다."); document.location.href="/write";</script>');
-  }
-})
+router.post('/writebooks', function(req, res){
+  // try{
+  
+  //   var writepost = {title : req.body.Title, writedate : writedate, name: req.session.Name, post: req.body.Post, field: req.body.Field, town: req.body.Town}
+
+  //   // console.log('제목 : ' + title + ', 수정 날짜 : ' + writedate + ', 닉네임: ' + name + ', 내용: ' + post + ', 분류: ' + field + ', 지역: ' + town);
+
+  //   post.insertOne(writepost);
+  //   res.send('<script type="text/javascript">alert("글쓰기가 완료되었습니다."); document.location.href="/";</script>');
+  // }
+  // catch(exception){
+  //   res.send('<script type="text/javascript">alert("글쓰기를 실패하였습니다."); document.location.href="/writebooks";</script>');
+  // }
+  var title = req.body.title || req.query.title;
+  var date = new Date();
+  var writedate = String(date.getFullYear()) + '년 ' + String(date.getMonth() + 1) + '월 ' + String(date.getDate()) + '일';
+  var name = req.body.name || req.query.name;
+  var post = req.body.post || req.query.post;
+  var field = req.body.field || req.query.field;
+  var town = req.body.town || req.query.town;
+  console.log('제목 : ' + title + ', 날짜 : ' + writedate + ', 이름: ' + name + ', 내용: ' + post + ', 분류: ' + field + ', 지역: ' + town);
+
+      if (db) {
+        db.addpost(title, writedate, name, post, field, town,
+          function (err, result) {
+            if (err) {
+              console.log(err);
+              res.send('<script type="text/javascript">alert("에러가 발생했습니다.")document.location.href="/";</script>');
+              res.end();
+              return;
+            }
+
+            if (result) {
+              console.log("글쓰기 성공!");
+              res.send('<script type="text/javascript">alert("글이 정상적으로 등록되었습니다!"); document.location.href="/";</script>');
+              res.end();
+              return;
+            } else {
+              console.log('문제 발생!');
+              res.send('<script type="text/javascript">alert("추가에 실패했습니다.")document.location.href="/";</script>');
+              res.end();
+              return;
+            }
+          }
+        );
+      } else {
+        console.log('DB 연결 안됨');
+        res.send('<script type="text/javascript">alert("DB가 연결되어 있지 않습니다!"); document.location.href="/";</script>');
+        res.end();
+        return;
+      }
+});  //글 작성
 
 module.exports = router;
