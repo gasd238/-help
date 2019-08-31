b6371b2d0b97a5790e2e7885a742e17755cd134e
ParkJongHyo <s18052@gsm.hs.kr>
2019-09-01 02:01:43 +0900

    많이 수정

--- routes/index.js
+++ routes/index.js
@@ -42,37 +42,34 @@ router.get('/adminpage', function(req, res){
   })
 });
 
+// router.post('/mypage', (req, req, next)=>{
+//   logindb.profile(req.session.user_id, (err, docs)=>{
+//     if(err){
+//       console.log(err.message);
+//     }else{
+      
+//     }
+//   })
+// })
+
 router.get('/mypage', function(req, res){
   if (req.session.user_id != null) {
-    var id = req.session.user_id;
-    logindb.profile(req.session.user_id, function (err, data) {
-      if (err) {
-        console.log(err);
-      }
-      if (data) {
-        req.session.name = data.name;
-        console.log(req.session.name);
-      }
-    });
     var post_count = 0;
-    console.log(id + ", " + req.session.name);
-    logindb.profile(id, function (err, show) {
+    logindb.profile(req.session.user_id, function (err, show) {
       if (err) {
         console.log("logindb: " + err);
         res.send('<script type="text/javascript">alert("에러가 발생했습니다."); document.location.href="/";</script>');
         res.end();
         return;
       }
-      postdb.getmypost(req.session.name, function (err, data){
+      postdb.getmypost(req.session.user_id, function (err, data){
         if(data){
           for(i=0; i < data.length; i++){
             post_count += 1;
           }
-          console.log("이름: " + show.name + ", 글 개수: " + post_count);
-        }else{
-          console.log("이름: " + show.name + ", 글 개수: " + 0);
         }
-        res.render('../views/User/MyPage.ejs', { post: data, name: show.name, islogin: 'login' });
+        console.log("이름: " + show.name + ", 글 개수: " + post_count);
+        res.render('../views/User/MyPage.ejs', { post: data, PInfo: show, islogin: 'login' });
         res.end();
       });
     });
@@ -99,7 +96,7 @@ router.post('/writebooks', function(req, res){
       console.log(err);
     }
     if (data) {
-      console.log('제목 : ' + title + ', 날짜 : ' + writedate + ', 이름: ' + data[0].name + ', 내용: ' + post + ', 분류: ' + field + ', 지역: ' + town);
+      console.log('제목 : ' + title + ', 날짜 : ' + writedate + ', 이름: ' + data.name + ', 내용: ' + post + ', 분류: ' + field + ', 지역: ' + town);
 
       if (postdb) {
         postdb.addpost(title, writedate, data.name, post, field, town,
