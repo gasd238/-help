508150e8e8615f9319b0e988bbfecead316cd84a
JEONG-MW <fwwfly@gmail.com>
2019-08-31 09:59:35 +0900

    .
    .

--- public/js/writepost.js
+++ public/js/writepost.js
@@ -3,16 +3,32 @@
 function getLocation() {
   if (navigator.geolocation) {
     //위치 정보를 얻기
-    navigator.geolocation.getCurrentPosition (function(pos) {
-        $('#latitude').html(pos.coords.latitude);     // 위도
-        $('#longitude').html(pos.coords.longitude); // 경도
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      $('#latitude').html(pos.coords.latitude);     // 위도
+      $('#longitude').html(pos.coords.longitude); // 경도
     });
-  // if (navigator.geolocation) {
-  //   navigator.geolocation.getCurrentPosition(currentPosition, geoError);
-  // }
-  // else {
-  //   w.innerHTML = "지리위치를 지원하지 않는 브라우저 입니다.";
-  // }
+    //   $.ajax({
+    //     url: 'https://apis.daum.net/local/geo/coord2addr?apikey=faskljalksfjaklfnalksd&longitude=' + longitude + '&latitude=' + latitude + '&inputCoordSystem=WGS84&output=json',
+    //     type: 'GET',
+    //     cache: false,
+    //     context: {},
+    //     crossOrigin: true,
+    //     success: function(data) {
+    //         var jsonObj = $.parseJSON(data);
+    //         var contentText = document.getElementById('content');
+    //             contentText.innerHTML += "<br>동네 이름 : " + jsonObj.name;
+    //             $('#dongne').html(contentText);
+    //     },error:function(request,status,error){
+    //         alert("다시 시도해주세요.\n" + "code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+    //     }
+    // });
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(currentPosition, geoError);
+    // }
+    // else {
+    //   w.innerHTML = "지리위치를 지원하지 않는 브라우저 입니다.";
+    // }
+  }
 }
 function currentPosition(myPosition) {
   var a = myPosition.coords.latitude + "," + myPosition.coords.longitude;
