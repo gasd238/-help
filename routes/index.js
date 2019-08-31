f40e2cc9c1e28ca93770ea6f58d25cb435e3b56c
gasd238 <gasd238@naver.com>
2019-08-31 15:58:41 +0900

    어드민 페이지 대충 만듬

--- routes/index.js
+++ routes/index.js
@@ -37,7 +37,6 @@ router.get('/test2', function (req, res) {
 
 router.get('/adminpage', function(req, res){
   logindb.allprofile((err, data)=>{
-    console.log(data)
     res.render('../views/User/administerpage.ejs', {profile: data})
   })
 });
